fe82aa6359fa9d7cab31ac150a9270a8cbd443dc
Ryan Van Etten <ryanve@gmail.com>
2014-01-06 05:05:02 -0500

    Use 2-space indentation

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 (function(root) {
-    var common = typeof module != 'undefined' && !!module.exports;
-    var aok = common ? require('../node_modules/aok') : root.aok;
-    aok({ id:'example', test:true });
+  var common = typeof module != 'undefined' && !!module.exports;
+  var aok = common ? require('../node_modules/aok') : root.aok;
+  aok({ id:'example', test:true });
 }(this));

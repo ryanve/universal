aaf847fc6dc1a1edc147b1b6e20e063b431b52e8
Ryan Van Etten <ryanve@gmail.com>
2014-02-27 02:00:59 -0500

    Harden GruntFile.js

--- GruntFile.js
+++ GruntFile.js
@@ -1,12 +1,13 @@
 module.exports = function(grunt) {
   var _ = grunt.util._,
+    fs = require('fs'),
     pkg = grunt.file.readJSON('package.json'),
     path = require('path'),
     from = 'src/',
     main = pkg.main && path.basename(pkg.main) || 'index.js',
     source = [_.find([from + 'index.js', from + main, from + pkg.name], function(v) {
-      return this.existsSync(v);
-    }, require('fs'))],
+      return fs.existsSync(v);
+    })],
     holder = (function(who) {
       return typeof who == 'string' ? who.split(/\s+</)[0].trim() : who && who.name || '';
     }(pkg.author));
@@ -26,7 +27,7 @@ module.exports = function(grunt) {
       build: [main],
       options: {
         ignores: ['**/**/node_modules/', '**/**/vendor/', '**/**.min.js'],
-        debug:true, expr:true, sub:true, boss:true, supernew:true, node:true, 
+        debug:true, expr:true, sub:true, boss:true, supernew:true, node:true,
         undef:true, unused:true, devel:true, evil:true, laxcomma:true, eqnull:true, 
         browser:true, globals:{ender:true, define:true}, jquery:true, maxerr:10
       }
@@ -37,7 +38,7 @@ module.exports = function(grunt) {
           '/*!',
           ' * <%= pkg.name %> <%= pkg.version %>+<%= grunt.template.today("UTC:yyyymmddHHMM") %>',
           ' * <%= pkg.homepage %>',
-          ' * MIT License <%= grunt.template.today("UTC:yyyy") %> ' + holder,
+          ' * MIT License (c) <%= grunt.template.today("UTC:yyyy") %> ' + holder,
           ' */\n\n'
         ].join('\n')
       },
@@ -56,9 +57,9 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.loadNpmTasks('aok');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.registerTask('default', ['jshint:grunt', 'jshint:sub', 'concat', 'jshint:build', 'uglify']);
+  fs.existsSync('tasks') && grunt.loadTasks('tasks');
+  _.keys(pkg.devDependencies).some(function(name) {
+    this.test(name) && grunt.loadNpmTasks(name);
+  }, /^grunt-|aok/);
+  grunt.registerTask('default', ['jshint:grunt', 'jshint:sub', 'aok', 'concat', 'jshint:build', 'uglify']);
 };

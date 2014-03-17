63446d6788e02e10b8ebba6bffebc02ff8349f8c
Ryan Van Etten <ryanve@gmail.com>
2014-03-17 15:24:03 -0400

    Let uglify be optional

--- GruntFile.js
+++ GruntFile.js
@@ -1,6 +1,5 @@
 //github.com/ryanve/universal#grunt
-//gruntjs.com/configuring-tasks#globbing-patterns
-module.exports = function(grunt) {
+module.exports = function(grunt) {  
   var _ = grunt.util._;
   var fs = require('fs');
   var exists = _.compose(fs.existsSync.bind(fs), _.identity);
@@ -9,12 +8,16 @@ module.exports = function(grunt) {
   var main = pkg.main && path.basename(pkg.main) || 'index.js';
   var srcs = [_.find(['src/index.js', 'src/' + main, 'src/' + pkg.name], exists)];
 
+  function maybe(task) {
+    return grunt.task._tasks[task] ? [task] : [];
+  }
+
   grunt.initConfig({
     pkg: pkg,
     jshint: {
+      all: ['./'], // deep
       dir: ['*.js'], // shallow
       sub: ['*/'], // sub dirs
-      all: ['./'], // deep
       src: ['src/'],
       test: ['test/'],
       grunt: [path.basename(__filename)],
@@ -45,13 +48,12 @@ module.exports = function(grunt) {
     }
   });
 
-  // Load any local tasks and then load npm ones
   exists('tasks') && grunt.loadTasks('tasks');
   _.keys(pkg.devDependencies).some(function(name) {
     this.test(name) && grunt.loadNpmTasks(name);
   }, /^grunt-|aok/);
 
-  grunt.registerTask('test', ['jshint:sub'].concat(pkg.devDependencies.aok ? 'aok' : []));
-  grunt.registerTask('build', ['jshint:grunt', 'test', 'concat:build', 'jshint:build', 'uglify']);
+  grunt.registerTask('test', ['jshint:sub', 'jshint:grunt'].concat(maybe('aok')));
+  grunt.registerTask('build', ['test', 'concat:build', 'jshint:build'].concat(maybe('uglify')));
   grunt.registerTask('default', ['build']);
 };

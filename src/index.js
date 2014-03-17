89b91ea156f5994b56828153d50d6129207a4e65
Ryan Van Etten <ryanve@gmail.com>
2014-03-17 14:13:04 -0400

    Allow build tools to mangle .exports and .amd

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 !function(root, name, make) {
-  if (typeof module != 'undefined' && module['exports']) module['exports'] = make();
-  else if (typeof define == 'function' && define['amd']) define(make);
+  if (typeof module != 'undefined' && module.exports) module.exports = make();
+  else if (typeof define == 'function' && define.amd) define(make);
   else if (!name) make();
   else root[name] = make();
 }(this, '', function() {

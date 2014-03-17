999ebf2892bbf68c60e88fce002254d573e1b95b
Ryan Van Etten <ryanve@gmail.com>
2014-03-17 13:46:00 -0400

    Initiate with a failing test

--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 (function(root) {
   var common = typeof module != 'undefined' && !!module.exports;
   var aok = common ? require('aok') : root.aok;
-  aok({ id:'example', test:true });
+  var api = common ? require('../src') : root['put export name here'];
+  aok('export exists', !!api);
 }(this));

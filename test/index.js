e8cb20d76954dbcaf5e2bdbbff49f633f8a6f326
Ryan Van Etten <ryanve@gmail.com>
2014-03-17 13:29:10 -0400

    Path is not needed to require in node

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 (function(root) {
   var common = typeof module != 'undefined' && !!module.exports;
-  var aok = common ? require('../node_modules/aok') : root.aok;
+  var aok = common ? require('aok') : root.aok;
   aok({ id:'example', test:true });
 }(this));

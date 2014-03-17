bc061be9fbcbea26d19c8cd2f6bab49cb04c2568
Ryan Van Etten <ryanve@gmail.com>
2014-03-17 15:22:27 -0400

    Fix banner

--- src/banner.js
+++ src/banner.js
@@ -1,5 +1,5 @@
 /*!
- * <%= pkg.name %> <%= pkg.version %>+<%= grunt.template.today("UTC:yyyymmddHHMM") %>',
- * <%= pkg.homepage %>',
- * <%= pkg.license %> License <%= grunt.template.today("UTC:yyyy") %> <%= pkg.author.name || pkg.author.split(/\s*(<|\()/)[0] %>',
+ * <%= pkg.name %> <%= pkg.version %>+<%= grunt.template.today("UTC:yyyymmddHHMM") %>
+ * <%= pkg.homepage %>
+ * <%= pkg.license %> License, <%= grunt.template.today("UTC:yyyy") %> <%= pkg.author.name || pkg.author.split(/\s*(<|\()/)[0] %>
  */

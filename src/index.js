3bb7e79bd4a8259e9a897741872f4abddffeabe8
Ryan Van Etten <ryanve@gmail.com>
2014-03-17 14:09:42 -0400

    Use !bang closures rather than parens.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-(function(root, name, make) {
+!function(root, name, make) {
   if (typeof module != 'undefined' && module['exports']) module['exports'] = make();
   else if (typeof define == 'function' && define['amd']) define(make);
   else if (!name) make();
   else root[name] = make();
 }(this, '', function() {
 
-}));
+});
--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
-(function(root) {
+!function(root) {
   var common = typeof module != 'undefined' && !!module.exports;
   var aok = common ? require('aok') : root.aok;
   var api = common ? require('../src') : root['put export name here'];
   aok('export exists', !!api);
-}(this));
+}(this);

08f6e375fc6a6fd508280c7f1bddf91a5df7d53c
Ryan Van Etten <ryanve@gmail.com>
2014-04-02 13:27:30 -0400

    (c) applies to most licenses

--- root/src/banner.js
+++ root/src/banner.js
@@ -1,6 +1,6 @@
 /*!
  * <%= pkg.name %> <%= pkg.version %>+<%= grunt.template.today("UTC:yyyymmddHHMM") %>
  * <%= pkg.homepage %>
- * <%= pkg.license %> License, <%= grunt.template.today("UTC:yyyy") %> <%= 
+ * <%= pkg.license %> License (c) <%= grunt.template.today("UTC:yyyy") %> <%= 
       pkg.author.name || pkg.author.split(/\s*(<|\()/)[0] %>
  */

fc9b509511aea82f0b7a0ac743a564f421eba552
Ryan Van Etten <ryanve@gmail.com>
2015-02-21 01:52:18 -0800

    Don't deal with year confusion here

--- root/src/banner.js
+++ root/src/banner.js
@@ -1,6 +1,5 @@
 /*!
  * <%= pkg.name %> <%= pkg.version %>+<%= grunt.template.today("UTC:yyyymmddHHMM") %>
  * <%= pkg.homepage %>
- * <%= pkg.license %> License (c) <%= grunt.template.today("UTC:yyyy") %> <%= 
-      pkg.author.name || pkg.author.split(/\s*(<|\()/)[0] %>
+ * <%= pkg.license %> License (c) <%= pkg.author.name || pkg.author.split(/\s*(<|\()/)[0] %>
  */

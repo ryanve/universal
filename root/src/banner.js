c22682ea448887e7db32b09ecf1ad1fda7d8bb97
Ryan Van Etten <ryanve@gmail.com>
2015-08-23 21:09:57 -0700

    remove name from here to prevent being in forks

--- root/src/banner.js
+++ root/src/banner.js
@@ -1,5 +1,5 @@
 /*!
  * <%= pkg.name %> <%= pkg.version %>+<%= grunt.template.today("UTC:yyyymmddHHMM") %>
  * <%= pkg.homepage %>
- * <%= pkg.license %> License (c) <%= pkg.author.name || pkg.author.split(/\s*(<|\()/)[0] %>
+ * @license <%= pkg.license %>
  */
